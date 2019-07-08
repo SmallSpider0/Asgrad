f7cf13a06378d34d29a34a76a7de1cd8233c5bda
SmallSpider0 <568442079@qq.com>
2019-07-08 17:33:47 +0800

    尝试修复跨域问题

--- src/my-lib/tools.php
+++ src/my-lib/tools.php
@@ -1,5 +1,10 @@
 <?php
-header('Access-Control-Allow-Origin:*');
+// 制定允许其他域名访问
+header("Access-Control-Allow-Origin:*");
+// 响应类型
+header('Access-Control-Allow-Methods:POST');
+// 响应头设置
+header('Access-Control-Allow-Headers:x-requested-with, content-type');
 
 require_once Root_Path . '/config.php';
 require_once Root_Path . '/input_config.php';
